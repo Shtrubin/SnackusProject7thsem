ee52e504b948ff3728820cb0fab53813b574d4fd
Shtrubin <rubeensht17@gmail.com>
2024-11-17 14:51:40 +0545

    separated json data

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,37 +1,7 @@
 import React from "react";
 import RestaurantCard from "../components/RestaurantCard";
 import '../styles/home.css'
-
-const restaurants = [
-  {
-    restaurantName: "The Gourmet Bistro",
-    location: "New York, NY",
-    description: "A modern bistro offering a fusion of American and European cuisine.",
-    rate: 4.5,
-    restaurantPhotoUrl: "https://picsum.photos/200/300",
-  },
-  {
-    restaurantName: "Pasta Paradise",
-    location: "Los Angeles, CA",
-    description: "An Italian restaurant specializing in fresh, handmade pasta and traditional dishes.",
-    rate: 4.7,
-    restaurantPhotoUrl: "https://picsum.photos/200/300",
-  },
-  {
-    restaurantName: "Sushi Haven",
-    location: "San Francisco, CA",
-    description: "Authentic sushi and sashimi served in a contemporary, minimalist setting.",
-    rate: 4.9,
-    restaurantPhotoUrl: "https://picsum.photos/200/300",
-  },
-  {
-    restaurantName: "Vegan Delights",
-    location: "Austin, TX",
-    description: "A plant-based eatery offering creative and delicious vegan dishes.",
-    rate: 4.8,
-    restaurantPhotoUrl: "https://picsum.photos/200/300",
-  },
-];
+import restaurants from "../restaurantData";
 
 function Home() {
   return (
