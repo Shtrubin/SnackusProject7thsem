ad9131a5fc28b798f9e63da4bd8dd702ceb68129
Shtrubin <rubeensht17@gmail.com>
2024-11-30 19:05:25 +0545

    added navbar photo

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,6 +1,7 @@
 import "../styles/navbar_styles.css";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import headAds from "../assets/headAds.png"
 
 const Navbar = () => {
   return (
@@ -15,7 +16,7 @@ const Navbar = () => {
             <h1>SnackUs</h1>
           </div>
           <div id="ad-image">
-            <img src="https://picsum.photos/200/300" alt="Advertisement" />
+            <img src={headAds} alt="Advertisement" />
           </div>
         </div>
         <div id="menus">
