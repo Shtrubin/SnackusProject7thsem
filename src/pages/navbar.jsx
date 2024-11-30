a7756e7879dbce548dfc28481831fe430c747422
Shtrubin <rubeensht17@gmail.com>
2024-11-30 18:46:56 +0545

    styling for navbar

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,5 +1,6 @@
 import "../styles/navbar_styles.css";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
@@ -8,13 +9,13 @@ const Navbar = () => {
         <div id="nav-top">
           <div id="logo">
             <img
-              src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?t=st=1723686274~exp=1723689874~hmac=7ace5166283ca85861a61b7f4f176518d0cb69ab6fc197716f1d5b3671102780&w=996"
+              src={logo}
               alt="Logo"
             />
             <h1>SnackUs</h1>
           </div>
           <div id="ad-image">
-            <img src="https://via.placeholder.com/350x100" alt="Advertisement" />
+            <img src="https://picsum.photos/200/300" alt="Advertisement" />
           </div>
         </div>
         <div id="menus">
