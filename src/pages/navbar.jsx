88383659d1858f86abf268955d10532205fae768
Shtrubin <rubeensht17@gmail.com>
2024-12-01 06:27:20 +0545

    added icon

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -2,6 +2,9 @@ import "../styles/navbar_styles.css";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import headAds from "../assets/headAds.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBowlFood , faHouse, faMugSaucer,faUtensils,faMagnifyingGlass,faRightFromBracket,faUserPlus,faRightToBracket} from '@fortawesome/free-solid-svg-icons';
+import { faRocketchat } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = ({ isLoggedIn }) => {
   const handleLogout = () => {
@@ -24,19 +27,19 @@ const Navbar = ({ isLoggedIn }) => {
         </div>
       </div>
       <div id="menus">
-        <p><Link to="/">Home</Link></p>
-        <p><Link to="/Local">Local</Link></p>
-        <p><Link to="/Mid-range">Mid-range</Link></p>
-        <p><Link to="/High-End">High-End</Link></p>
-        <p><Link to="/search">Search</Link></p>
-        <p><Link to="/chatbot">Chatbot</Link></p>
+        <p><Link to="/"><FontAwesomeIcon icon={faHouse} /> Home</Link></p>
+        <p><Link to="/Local"><FontAwesomeIcon icon={faBowlFood} /> Local</Link></p>
+        <p><Link to="/Mid-range"><FontAwesomeIcon icon={faMugSaucer} /> Mid-range</Link></p>
+        <p><Link to="/High-End"><FontAwesomeIcon icon={faUtensils} /> High-End</Link></p>
+        <p><Link to="/search"><FontAwesomeIcon icon={faMagnifyingGlass} /> Search</Link></p>
+        <p><Link to="/chatbot"><FontAwesomeIcon icon={faRocketchat} /> Chatbot</Link></p>
         {!isLoggedIn ? (
           <>
-            <p><Link to="/signup">SignUp</Link></p>
-            <p><Link to="/login">Login</Link></p>
+            <p><Link to="/signup"><FontAwesomeIcon icon={faUserPlus} /></Link></p>
+            <p><Link to="/login"><FontAwesomeIcon icon={faRightToBracket} /></Link></p>
           </>
         ) : (
-          <p onClick={handleLogout}>Logout</p>
+          <p onClick={handleLogout}><FontAwesomeIcon icon={faRightFromBracket} /></p>
         )}
       </div>
     </div>
